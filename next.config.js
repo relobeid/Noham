649eb94ec5416692bd36476416b89603d73f02a5
relobeid <ramezelobeid@icloud.com>
2025-07-04 15:50:33 -0400

    configs

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['avatars.githubusercontent.com', 'github.com'],
+  },
+  webpack: (config) => {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig; 
